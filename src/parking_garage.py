7bb2214226111294020ea9885ab1a3cbf2f66f49
sromano87 <siromano@unisa.it>
2024-11-04 12:30:06 +0100

    Minor changes

--- src/parking_garage.py
+++ src/parking_garage.py
@@ -26,69 +26,47 @@ class ParkingGarage:
         GPIO.setup(self.SERVO_PIN, GPIO.OUT)
         GPIO.setup(self.LED_PIN, GPIO.OUT)
         self.rtc = RTC.RTC(self.RTC_PIN)
-        self.pwm = GPIO.PWM(self.SERVO_PIN, 50)
-        self.pwm.start(0)
+        self.servo = GPIO.PWM(self.SERVO_PIN, 50)
+        self.servo.start(0)
         self.door_open = False
         self.light_on = False
 
     def check_occupancy(self, pin: int) -> bool:
-        """
-        Checks whether one of the infrared distance sensor detects something in front of it.
-        :param pin: The pin of the sensor that is being checked (e.g., INFRARED_PIN1).
-        :return: True if the infrared sensor detects something, False otherwise.
-        :raise ParkingGarageError: if the pin is not that of any infrared distance sensors.
-        """
+        # To be implemented
+        pass
 
     def get_occupied_spots(self) -> int:
-        """
-        Calculates the number of occupied parking spots in the garage.
-        :return: The number of occupied spots.
-        """
+        # To be implemented
+        pass
 
     def calculate_parking_fee(self, entry_time: str) -> float:
-        """
-        Calculates the amount of money to be paid by the customer of the garage.
-        For each hour spent in the garage, there is a flat cost of 2.50€;
-        additionally, during the weekend (Saturday and Sunday)
-        an additional 25% fee is applied to the total of the parking ticket.
-        Even when customers do not exceed a full hour, they will still be charged 2.50 €.
-        :param entry_time: A string in the format "hh:mm:ss" containing the entry time of a
-        vehicle in the garage
-        :return: The total amount to be paid by the customer
-        """
+        # To be implemented
+        pass
 
     def open_garage_door(self) -> None:
-        """
-        Opens the garage door using the servo motor
-        :raise ParkingGarageError: if the door is already open
-        """
+        # To be implemented
+        pass
 
     def close_garage_door(self) -> None:
-        """
-        Closes the garage door using the servo motor
-        :raise ParkingGarageError: if the door is already closed
-        """
+        # To be implemented
+        pass
 
     def turn_light_on(self) -> None:
-        """
-        Turns on the smart lightbulb
-        """
+        # To be implemented
+        pass
 
     def turn_light_off(self) -> None:
-        """
-        Turns off the smart lightbulb
-        """
+        # To be implemented
+        pass
 
     def change_servo_angle(self, duty_cycle):
         """
-        Changes the servo motor's angle by passing him the corresponding PWM duty cycle signal
-        :param duty_cycle: the length of the duty cycle
+        Changes the servo motor's angle by passing it the corresponding PWM duty cycle
+        :param duty_cycle: the width of the PWM duty cycle (it's a percentage value)
         """
-        GPIO.output(self.SERVO_PIN, GPIO.HIGH)
-        self.pwm.ChangeDutyCycle(duty_cycle)
+        self.servo.ChangeDutyCycle(duty_cycle)
         time.sleep(1)
-        GPIO.output(self.SERVO_PIN, GPIO.LOW)
-        self.pwm.ChangeDutyCycle(0)
+        self.servo.ChangeDutyCycle(0)
 
 
 class ParkingGarageError(Exception):
--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
 from unittest.mock import patch
 from unittest.mock import Mock
+from mock import GPIO
+
 
 class TestParkingGarage(TestCase):
 
-    @patch.object(SomeClassOrModule, 'someCallable')
-    def test_something(self, mock_method: Mock):
-        # Your test
+    @patch.object(GPIO, "input")
+    def test_something(self, mock_object: Mock):
+        # This is an example of test where I want to mock the GPIO.input() function
+        pass
 
